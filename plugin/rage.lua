f78cdcedb865e53547889dad16ebab25148e3a35
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-08-13 17:26:29 -0700

    Update rage.lua

--- plugin/rage.lua
+++ plugin/rage.lua
@@ -1,5 +1,5 @@
-if vim.fn.has("nvim-0.7.0") ~= 1 then
-  vim.api.nvim_err_writeln("rage.nvim requires at least nvim-0.7.0. (i think)")
+if vim.fn.has("nvim-0.5.0") ~= 1 then
+  vim.api.nvim_err_writeln("rage.nvim requires at least nvim-0.5.0. (i think)")
 end
 
 vim.cmd("Rage")
